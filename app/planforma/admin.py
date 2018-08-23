2245aafc5b54c0b42cbe73189fe45eef8ef5494d
jm-begon <jm.begon@ulg.ac.be>
2018-08-23 17:37:42 +0200

    ENH Textarea for advice in admin zone

--- app/planforma/admin.py
+++ app/planforma/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+
+from planforma.forms import AdviceForm
 from .models import Field, Skill, Module, Criterion
 
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['name', 'fields', 'advices']
+    form = AdviceForm
+
+
 admin.site.register(Field)
-admin.site.register(Skill)
+admin.site.register(Skill, SkillAdmin)
 admin.site.register(Module)
 admin.site.register(Criterion)
+
+
---
+++ app/planforma/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Skill
+
+
+class AdviceForm(forms.ModelForm):
+    advices = forms.CharField(widget=forms.Textarea(attrs={'rows': 5,
+                                                           'cols': 100}))
+
+    class Meta:
+        model = Skill
+        fields = ('__all__')
+
